fac35c2fd3970ef6a23a88370033d59a18f5dddd
Thomas Jefferson <TOMartilence>
2025-01-22 18:00:14 +0530

    first backend commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const cors = require("cors")
+
+
+const app = express();
+app.use(cors())
+app.listen(5000,()=>{
+    console.log("The backend is live on port 5000");
+})
