2a91afabd55079e4a1a12f7813581180d5d9166f
Thomas Jefferson <TOMartilence>
2025-01-22 18:02:24 +0530

    abujsidf

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const cors = require("cors")
 const app = express();
 app.use(cors())
 app.listen(5000,()=>{
-    console.log("The backend is live on port 5000");
+    console.log("The backend live on port 5000");
 })
